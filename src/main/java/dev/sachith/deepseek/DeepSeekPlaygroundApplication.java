588bd57272e23dba39b8985a197f748f0d66eff3
Sachith <sachithl@airretailer.com>
2025-01-31 18:04:44 +0400

    bootstrap

---
+++ src/main/java/dev/sachith/deepseek/DeepSeekPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package dev.sachith.deepseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepSeekPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepSeekPlaygroundApplication.class, args);
+    }
+
+}
